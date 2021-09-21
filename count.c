c169a947bf05901e8845e583dbed37569a4518ee
mpourmpoulis <35875229+mpourmpoulis@users.noreply.github.com>
2021-09-21 23:57:58 +0300

    'c'

--- count.c
+++ count.c
@@ -2,7 +2,7 @@
   requires \valid_read(x + (0 .. N-1));
   requires N >= 0 ;
     
-  assigns best,i ;
+  assigns \nothing ;
   ensures 0 <= \result < N ;
   
   ensures \exists integer i ; \forall integer j; 0 <= j < \result ==>  x[i+j] == x[i] ;
@@ -11,8 +11,23 @@
 */
 int countSameConsecutive(int N, int x[]) {
 	int best = 0, i = 0;
+	/*@
+    loop invariant 0<= i <= N ;
+    loop invariant ( 0<i<N ) ==> x[i]    != x[i-1]
+     
+    loop assigns i,best ;
+    loop variant N-i ;
+  */
 	while (i < N) {
 	int j = i+1;
+
+	/*@
+    loop invariant i< j <= N ;
+    loop invariant 
+      \forall integer k ; i <= k < j ==> x[i]==x[k] ;
+    loop assigns j ;
+    loop variant N-j ;
+  	*/
 	while (j < N && x[j] == x[i]) ++j;
 	if (j-i > best) best = j-i;
 		i = j;
